514c81b299dce2a11f206b7b6a189ffbd5d397db
Murad Chowhdury <murad@biot22-144-dhcp.int.colorado.edu>
2024-04-26 13:50:19 -0600

    make index script

---
+++ make_index.py
@@ -0,0 +1,77 @@
+import argparse
+
+import faiss
+import numpy as np
+import pyfastx
+import torch
+from functool import partial
+from torch.utils.data import DataLoader
+from transformers import AutoModel, AutoTokenizer
+
+from dataloaders import FastqDataset, tokenize_batch
+
+
+def load_model(checkpoint: str):
+    model = AutoModel.from_pretrained(
+        checkpoint, device_map="auto", trust_remote_code=True
+    )
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
+    return model, tokenizer
+
+
+def main(checkpoint, index_path, dim, fastq, batch_size):
+    ## prepare model and data ----------------------------------------------
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model, tokenizer = load_model(checkpoint)
+    model.eval()
+
+    dataset = FastqDataset(fastq)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        collate_fn=partial(tokenize_batch, tokenizer=tokenizer),
+    )
+
+    ## build index ---------------------------------------------------------
+    index = faiss.IndexFlatL2(dim)
+    with torch.no_grad():
+        for batch in dataloader:
+            embeddings = torch.mean(model(batch.to(device)).last_hidden_state, dim=1)
+            index.add(embeddings.cpu().numpy())
+
+    faiss.write_index(index, index_path)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--checkpoint",
+        type=str,
+        default="LongSafari/hyenadna-medium-160k-seqlen-hf",
+        help="checkpoint to load the model and tokenizer",
+    )
+    parser.add_argument(
+        "--index_path",
+        type=str,
+        default="index.faiss",
+        help="path to save the index",
+    )
+    parser.add_argument(
+        "--dim",
+        type=int,
+        default=256,
+        help="dimension of the embedding vectors",
+    )
+    parser.add_argument(
+        "--fastq",
+        type=str,
+        required=True,
+        help="fastq file to index",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=512,
+        help="batch size for indexing",
+    )
+    return parser.parse_args()
