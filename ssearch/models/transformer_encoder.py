2f3a34005b404a00e9f02a328bad1ecc9d77f9d1
Murad <murad@thisisnotanemailaddress.com>
2024-10-17 13:51:41 -0600

    add adapter name

--- ssearch/models/transformer_encoder.py
+++ ssearch/models/transformer_encoder.py
@@ -14,6 +14,7 @@ class TransformerEncoder(torch.nn.Module):
         self.model = IA3Model(
             AutoModelForMaskedLM.from_pretrained(model_version, trust_remote_code=True),
             ia3_config,
+            adapter_name="nucleotide-transformer-ia3-ssearch",
         )
 
     def forward(self, input_ids, attention_mask):
