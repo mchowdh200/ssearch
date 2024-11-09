b98e55a9effa1880d6e0a1322f757229caec60d8
Murad <murad@thisisnotanemailaddress.com>
2024-11-09 14:53:04 -0700

    add sbatch script for knn-reference experiment

---
+++ knn-reference.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+#SBATCH --job-name=knn-reference
+#SBATCH --partition=nvidia-a100
+#SBATCH --gres=gpu:4
+#SBATCH --time=24:00:00
+#SBATCH --mem=500GB
+#SBATCH --cpus-per-task=32
+#SBATCH --nodes=1
+#SBATCH --output=logs/knn-reference.out
+#SBATCH --error=logs/knn-reference.err
+
+set -e
+
+source ${HOME}/miniforge3/bin/activate
+
+python ./ssearch/experiments/knn_reference_small.py
+
+mkdir -p /scratch/Shares/layer/projects/sequence_similarity_search/knn-reference-small
+cp /cache/much8161-results/index.faiss \
+   /scratch/Shares/layer/projects/sequence_similarity_search/knn-reference-small/index.faiss
+
